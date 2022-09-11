da1f031894ecfdfbf39bb46542e068a3593a70f8
AlperenAydın <55955068+aydnalperen@users.noreply.github.com>
2022-09-11 21:51:20 +0300

    add isverified column

--- models/user.go
+++ models/user.go
@@ -10,11 +10,12 @@ import (
 
 type User struct {
 	gorm.Model
-	Email    string `gorm:"size:255;not null;unique" json:"email"`
-	Name     string `gorm:"size:255;not null;unique" json:"name"`
-	Lastname string `gorm:"size:255;not null;unique" json:"lastname"`
-	Username string `gorm:"size:255;not null;unique" json:"username"`
-	Password string `gorm:"size:255;not null;unique" json:"password"`
+	Email      string `gorm:"size:255;not null;unique" json:"email"`
+	Name       string `gorm:"size:255;not null;unique" json:"name"`
+	Lastname   string `gorm:"size:255;not null;unique" json:"lastname"`
+	Username   string `gorm:"size:255;not null;unique" json:"username"`
+	Password   string `gorm:"size:255;not null;unique" json:"password"`
+	IsVerified int    `gorm:"size:255;not null;unique; default:0" json:"is_valid"`
 }
 
 func VerifyPassword(password, hashedPassword string) error {
